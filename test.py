9f7d86553a7c8d4529e7a152f3b8c370ea7a116a
Daisywait <waitwhat5656@163.com>
2025-10-03 19:12:56 +0800

    更新数据集路径和模型加载逻辑，优化代码结构

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ import networks.resnet as resnet
 import numpy as np
 import random
 import random
+from collections import OrderedDict
 def seed_torch(seed=1029):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -23,35 +24,53 @@ def seed_torch(seed=1029):
     torch.backends.cudnn.enabled = False
 seed_torch(100)
 DetectionTests = {
-                'ForenSynths': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/ForenSynths/',
-                                 'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
-                                 'no_crop'    : True,
-                               },
-
-           'GANGen-Detection': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/GANGen-Detection/',
-                                 'no_resize'  : True,
-                                 'no_crop'    : True,
-                               },
-
-         'DiffusionForensics': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/DiffusionForensics/',
-                                 'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
-                                 'no_crop'    : True,
-                               },
-
-        'UniversalFakeDetect': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/UniversalFakeDetect/',
-                                 'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
-                                 'no_crop'    : True,
-                               },
-
-                 }
+  'ForenSynths': {
+    'dataroot': './dataset/ForenSynths/',
+    'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+    'no_crop': True,
+  },
+  'GANGen-Detection': {
+    'dataroot': './dataset/GANGen-Detection/',
+    'no_resize': True,
+    'no_crop': True,
+  },
+  'DiffusionForensics': {
+    'dataroot': './dataset/DiffusionForensics/',
+    'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+    'no_crop': True,
+  },
+  'UniversalFakeDetect': {
+    'dataroot': './dataset/UniversalFakeDetect/',
+    'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+    'no_crop': True,
+  },
+  'Chameleon': {
+    'dataroot': './dataset/Chameleon/',
+    'no_resize': False,  # Due to the different shapes of images in the dataset, resizing is required during batch detection.
+    'no_crop': True,
+  },
+}
 
 
 opt = TestOptions().parse(print_options=False)
 print(f'Model_path {opt.model_path}')
-
+opt.model_path = "/root/NPR-DeepfakeDetection/NPR.pth"  # 绝对路径测试
+print(opt.model_path)
 # get model
 model = resnet50(num_classes=1)
-model.load_state_dict(torch.load(opt.model_path, map_location='cpu'), strict=True)
+#model.load_state_dict(torch.load(opt.model_path, map_location='cpu'), strict=True)
+#checkpoint = torch.load(opt.model_path, map_location='cpu')
+#model.load_state_dict(checkpoint['model'], strict=True)
+checkpoint = torch.load(opt.model_path, map_location='cpu')
+state_dict = checkpoint['model']  # 或 checkpoint 本身，如果是整个 dict
+# 去掉 DataParallel 的 "module." 前缀
+from collections import OrderedDict
+new_state_dict = OrderedDict()
+for k, v in state_dict.items():
+    name = k.replace("module.", "")  # 去掉 module.
+    new_state_dict[name] = v
+
+model.load_state_dict(new_state_dict, strict=True)
 model.cuda()
 model.eval()
 
